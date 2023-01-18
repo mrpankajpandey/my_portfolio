5624deb6037c19a28afd0aa2f2a293f203ff377d
mrpankajpandey <107976020+mrpankajpandey@users.noreply.github.com>
2023-01-18 12:11:44 +0530

    Update scriprt.js

--- js/scriprt.js
+++ js/scriprt.js
@@ -68,7 +68,7 @@ totalSection = allSection.length;
                 }
             }
     /* preloader */
-    var loader= document.getElementById("preloader");
+    var loader= document.getElementById("loader");
     window.addEventListener("load", function(){
         loader.style.display="none";
     })
