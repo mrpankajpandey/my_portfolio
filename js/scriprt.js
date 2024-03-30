dabbcee37fc4b8f932e893c36cbfe2dca36fc868
mrpankajpandey <mr.pankajpandey0038@gmail.com>
2024-03-30 12:09:11 +0530

    Updated

--- js/scriprt.js
+++ js/scriprt.js
@@ -1,12 +1,12 @@
 /* ==============typing animatiion=========== */
 var typed =new Typed(".typing" ,{
-    strings:["","Web Designer ", "Android Development", "Socail Media Handler","Portfolio Designer ", "Freelancer"],
+    strings:["","Web Designer ", "Android Development", "Full-stack web dev", "Freelancer"],
     typedSpeed:120,
     BackSpeed:100,
     loop:true
 } )
 var typed =new Typed(".ptyping" ,{
-    strings:["","Web Designer ", "Android Development", "Socail Media Handler","Portfolio Designer", "Freelancer"],
+    strings:["","Web Designer ", "Android Development", "Full-stack web dev", "Freelancer"],
     typedSpeed:120,
     BackSpeed:100,
     loop:true
